6b9714e519dbb7df2ff27bd637b77fc5787de356
grbcp5 <grbcp5@mst.edu>
2017-08-28 23:19:42 -0500

    Add Firebase

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+var admin = require( "firebase-admin" );
 var stdin = process.openStdin();
+
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://betasig-d4b96.firebaseio.com"
+        });
+
+var db = admin.database();
+var ref = db.ref("author");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
 
 stdin.addListener("data", function(d) {
         // note:  d is an object, and when converted to a string it will
